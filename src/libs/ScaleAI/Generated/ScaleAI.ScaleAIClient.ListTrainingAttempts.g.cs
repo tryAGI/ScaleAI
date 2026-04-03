41823ae512f1a1369932a9c5a12eb3cfe6cd3097
Konstantin S. <havendv@gmail.com>
2026-04-03 08:57:44 +0400

    feat: Updated OpenAPI spec (#7)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ScaleAI/Generated/ScaleAI.ScaleAIClient.ListTrainingAttempts.g.cs
+++ src/libs/ScaleAI/Generated/ScaleAI.ScaleAIClient.ListTrainingAttempts.g.cs
@@ -153,7 +153,6 @@ namespace ScaleAI
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsStringAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
